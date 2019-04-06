13d178bbe50cf43eec3e0614b8a94d57d28b1e35
Olivia Ea <ea.olivia1@gmail.com>
2019-04-06 12:40:26 -0700

    Login logic added

--- views.py
+++ views.py
@@ -13,3 +13,59 @@ def index():
     """ Homepage """
 
     return render_template('homepage.html')
+
+@app.route('/register-new-user', methods=['GET'])
+def register_new_user():
+
+    return render_template('register-new-user.html')
+
+@app.route('/sign-up-verification', methods=["POST"])
+def register_process():
+    """ Register New User """
+
+    username = request.form["new-username"]
+    password = request.form["new-password"]
+
+    user = User.query.filter(User.user_id==username).first()
+
+    if user:
+        # flash("That username is already taken!")
+        return redirect("/login-current-user")
+    else:
+        new_user = User(user_id=username, password=password, auth_token=session.get('access_token'), refresh_token=session.get('refresh_token'))
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        user_id = new_user.user_id
+        session['logged_user'] = {'username': username}
+
+        # flash(f"User {username} added.")
+
+        return redirect("/activity-page")
+
+@app.route('/login-current-user', methods=['GET'])
+def login_form():
+
+    return render_template("login-page.html")
+
+@app.route('/login-verification', methods=["POST"])
+def login_current_user():
+    """ Login for Exisiting User """
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = User.query.filter(User.user_id==username).first()
+
+    if user:
+        if user.password == password:
+            user_id = user.user_id
+            session['logged_user'] = {'username': user_id}
+            # flash("You've successfully logged in!")
+            return redirect("/shopping-page")
+        else:
+            return("The password is incorrect.")
+    else:
+        # flash("That username doesn't exist!")
+        return redirect("/register-new-user")
